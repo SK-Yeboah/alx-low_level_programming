4c4bdcd8e2be9264d96466d9b8e0154d7c5f6f42
SK-Yeboah <skyeboah9@gmail.com>
2023-08-31 12:20:01 -0700

    kernel docu

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,17 @@
 #index "main.h"
 
+/**
+ * binary_to_uint - a function that converts a binary number
+ *                  to an unsigned int
+ *
+ * @b: string that contains 0 and 1 characters
+ *
+ * Return: the number converted or 0
+ *         if @b contains a character
+ *         that is not 0 or 1 or when
+ *         @b is null
+*/
+
 unsigned int binary_to_unit(const *b)
 {
 	unsigned int bit;
