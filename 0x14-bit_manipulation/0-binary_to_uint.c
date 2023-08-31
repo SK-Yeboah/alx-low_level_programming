de16aeee7bcbe07c0e5c89b9f689d2e3a5108df2
SK-Yeboah <skyeboah9@gmail.com>
2023-08-31 14:21:56 -0700

    bi_to_unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,30 +7,24 @@
  * Return:converted number, or 0
  */
 
-unsigned int binary_to_unit(const *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int bit;
-	unsigned int result = 0;
-	int index = 0;
+	int i;
+	unsigned int j;
 
-	if (b == NULL)
-	{
+	j = 0;
+	if (!b)
 		return (0);
-	}
-
-	while (b[index] != '0' && b[index] != '1')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[index] != '0' && b[index] != '1')
-		{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		}
-
-		bit  = b[index] - '0';
-
-		result = (result << 1) | bit;
-		index++;
 	}
-
-return (result);
-
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
