be3f729b2658567dd8e0ba446549cd17b918ab75
SK-Yeboah <skyeboah9@gmail.com>
2023-09-05 11:28:21 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,57 +1,59 @@
 #include "main.h"
 
+
 void error_exit(int exit_code, const char *message)
 {
 	dprintf(STDERR_FILENO, "%s\n", message);
 	exit(exit_code);
 }
-
-
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-    const char *file_from;
-    const char *file_to;
+	int fd_r, fd_w, x, m, n;
+	char buf[BUFSIZ];
 
-    int fd_source, fd_dest;
-    ssize_t read_bytes, write_bytes;
-    char buffer[BUFFER_SIZE];
+	if (argc != 3)
+	{
+		error_exit(97,  "Usage: cp file_from file_to\n");
+	}
 
-    if (argc != 3)
-    {
-	    error_exit(97, "Usage: cp file_from file_to");
-    }
+	fd_r = open(argv[1], O_RDONLY);
+	if (fd_r < 0)
+	{
+		error_exit(98,  "Usage: cp file_from file_to\n");
+	}
 
-    file_from = argv[1];
-    file_to = argv[2];
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    fd_source = open(file_from, O_RDONLY);
-    if (fd_source == -1) {
-        error_exit(98, "Error: Can't read from file");
-    }
+	while ((x = read(fd_r, buf, BUFSIZ)) > 0)
+	{
+		if (fd_w < 0 || write(fd_w, buf, x) != x)
+		{
+			error_exit(99,  "Usage: cp file_from file_to\n");
+			close(fd_r);
+		}
+	}
 
-    fd_dest = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (fd_dest == -1) {
-        error_exit(99, "Error: Can't write to file");
-    }
+	if (x < 0)
+	{
+		error_exit(98,  "Usage: cp file_from file_to\n");
+	}
 
-    while ((read_bytes = read(fd_source, buffer, BUFFER_SIZE)) > 0) {
-        write_bytes = write(fd_dest, buffer, read_bytes);
-        if (write_bytes == -1) {
-            error_exit(99, "Error: Can't write to file");
-        }
-    }
-
-    if (read_bytes == -1) {
-        error_exit(98, "Error: Can't read from file");
-    }
-
-    if (close(fd_source) == -1) {
-        error_exit(100, "Error: Can't close fd");
-    }
-
-    if (close(fd_dest) == -1) {
-        error_exit(100, "Error: Can't close fd");
-    }
-
-    return 0;
+	m = close(fd_r);
+	n = close(fd_w);
+	if (m < 0 || n < 0)
+	{
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
+	return (0);
 }
