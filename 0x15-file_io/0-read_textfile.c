f5f2b2c8d0447c5e6e05a4faa5a4a521473230f6
SK-Yeboah <skyeboah9@gmail.com>
2023-09-04 17:14:35 -0700

    read_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,57 @@
 #include "main.h"
 
 /**
- * read_textfile - Read and print text from a file.
+ * read_textfile-Read and print text from a file.
  * @filename: The name of the file to read from.
  * @letters: The maximum number of letters to read and print.
- * 
+ *
  * Return: The number of letters successfully read and printed. 0 on error.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL)
-        return 0;
+ssize_t custom_read_textfile(const char *file_name, size_t num_letters)
+{
+	int file_descriptor;
+	int read_result, write_result;
+	char *buffer;
 
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-        return 0;
+	if (!file_name)
+	{
+		return (0);
+	}
 
-    char buffer[BUFSIZE];
-    ssize_t total_read = 0;
-    ssize_t bytes_read;
+	file_descriptor = open(file_name, O_RDONLY);
+	if (file_descriptor < 0)
+	{
+		return (0);
+	}
 
-    while (letters > 0 && (bytes_read = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-        ssize_t bytes_to_print = bytes_read > letters ? letters : bytes_read;
-        ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_to_print);
+	buffer = malloc(sizeof(char) * num_letters);
 
-        if (bytes_written < 0 || bytes_written != bytes_to_print) {
-            fclose(file);
-            return 0;
-        }
+	if (!buffer)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-        letters -= bytes_written;
-        total_read += bytes_written;
-    }
+	read_result = read(file_descriptor, buffer, num_letters);
 
-    fclose(file);
-    return total_read;
+	if (read_result < 0)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
+
+	buffer[read_result] = '\0';
+	close(file_descriptor);
+
+	write_result = write(STDOUT_FILENO, buffer, read_result);
+
+	free(buffer);
+
+	if (write_result < 0)
+	{
+		return (0);
+	}
+	return (write_result);
 }
-i
