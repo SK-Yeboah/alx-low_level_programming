f8c327d3731886220a13654f69923f6d6cbda261
SK-Yeboah <skyeboah9@gmail.com>
2023-09-28 15:46:32 -0700

    0-binary_to_unit_1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,18 +19,15 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] == '0')
+		if (b[i] == '0' || b[i] == '1')
 		{
-		}
-		else if (b[i] == '1')
-		{
-			result = (result << 1) | 1;
+			result = result * 2 + (b[i] - '0');
 		}
 		else
 		{
 			return (0);
 		}
 	}
-	return (result);	
+	return (result);
 
 }
